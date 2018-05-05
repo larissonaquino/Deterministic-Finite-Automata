d55aed3552d03b4d4eb3ca53d5b746fe93cc5f78
Larisson A <larisson_fnx@hotmail.com>
2018-05-04 23:40:35 -0300

    Add files via upload

---
+++ afd.c
@@ -0,0 +1,119 @@
+/*
+-----------------------------------------------
+developed by Lárisson Aquino
+Agosto, 27, 2017
+-----------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXSIZE 10000
+
+int main(){
+    char alfabeto[MAXSIZE], transicao[MAXSIZE], input[MAXSIZE], op;
+    int qtd_estados, estado_inicial, qtd_finais, i, j;
+    
+    printf("\nDesenvolvido por Larisson Aquino - FBUNI\nBrazil - Setembro, 2017\n\n");
+
+    printf("Informe o alfabeto: ");
+    gets(alfabeto);
+    int tam_alfabeto = strlen(alfabeto);
+
+    printf("Informe a quantidade de estados: ");
+    scanf("%d", &qtd_estados);
+    if(qtd_estados == 0){
+        printf("\nQuantidade invalida!\n");
+        exit(1);
+    }
+    int linhas = qtd_estados*qtd_estados*tam_alfabeto;
+    char matriz[linhas][4];
+
+    printf("Informe o estado inicial: ");
+    setbuf(stdin, NULL);
+    scanf("%d", &estado_inicial);
+    setbuf(stdin, NULL);
+
+    printf("Informe a quantidade de estados finais: ");
+    scanf("%d", &qtd_finais);
+    if(qtd_finais == 0){
+        printf("\nQuantidade invalida!\n");
+        exit(1);
+    }
+    int estados_finais[qtd_finais];
+
+    printf("Informe o(s) estado(s) final(is): ");
+    for(i=0; i<qtd_finais; i++)
+        scanf("%d", &estados_finais[i]);
+
+    printf("Informe as regras de transicao: ");
+    setbuf(stdin, NULL);
+    gets(transicao);
+
+    int contador=0;
+    for(i=0; i<linhas; i++){
+        matriz[i][0] = transicao[contador++];
+        matriz[i][1] = transicao[contador++];
+        matriz[i][2] = transicao[contador++];
+        contador++;
+    }
+/* //IMPRIME A MATRIZ DE TRANSIÇÃO
+    for(i=0; i<20; i++){
+        for(j=0; j<3; j++){
+            printf("%c ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+*/
+entrada:
+    printf("\nInforme sua entrada: ");
+    setbuf(stdin, NULL);
+    gets(input);
+
+    int aux_final = estado_inicial;
+    char *c, xchar;
+    for(i=0, contador=0; i<linhas; i++, contador++){
+        //c = &matriz[i][0];
+        xchar = matriz[i][0];
+        c = &xchar;
+        //printf("atoi: %d\n", atoi(c));
+        if(atoi(c) == aux_final){
+            //printf("cmp: %c == %c\n", input[contador], matriz[i][1]);
+            if(input[contador] == matriz[i][1]){
+                c = &matriz[i][2];
+                *(c+1) = '\0';
+                //printf("aux atual: %d\n", aux_final);
+                aux_final = atoi(c);
+                //printf("aux depois: %d\n", aux_final);
+                i=-1;
+            }else{
+                //printf("else 1\n");
+                contador--;
+                continue;}
+        }else{
+            //printf("else 2\n");
+            contador--;
+            continue;
+        }
+    }
+
+    for(i=0; i<qtd_finais; i++){
+        if(estados_finais[i] == aux_final){
+            printf("\nEntrada aceita!\n\n");
+            printf("Deseja testar outra entrada? [s/n]\n\n");
+            op = getch();
+            if(op == 's' || op == 'S')
+                goto entrada;
+            else
+                return 0;
+        }
+    }
+    printf("\nEntrada rejeitada!\n\n");
+    printf("Deseja testar outra entrada? [s/n]\n\n");
+    op = getch();
+    if(op == 's' || op == 'S')
+        goto entrada;
+    else
+        return 0;
+}
